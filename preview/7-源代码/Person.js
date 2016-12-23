7a043d7f07d83a79e4affddda0f51ba0b061bafc
kentuo <godenlove@163.com>
2016-12-23 18:20:42 +0800

    Add files via upload

---
+++ preview/7-源代码/Person.js
@@ -0,0 +1,13 @@
+(function(win) {
+	var arr = [],
+		push = arr.push,
+		join = arr.join;
+
+	var Person = function() {};
+	Person.prototype = {
+		// ...
+	};
+
+
+	win.itcast.Per = Person;
+})(window);
---
+++ preview/7-源代码/Student.js
@@ -0,0 +1,13 @@
+(function(itcastObj) {
+	var arr = [],
+		push = arr.push,
+		join = arr.join;
+
+	var Person = function() {};
+	Person.prototype = {
+		// ...
+	};
+
+
+	itcastObj.Per = Person;
+})(itcast);
