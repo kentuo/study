54e739412a2585d9c64afdd5902f6f7eb4317895
kentuo <godenlove@163.com>
2016-12-23 16:54:20 +0800

    Create day1-lesson2.js

---
+++ preview/day1-lesson2.js
@@ -0,0 +1,22 @@
+// DOM：Document Object Model 文档对象模型
+// 		学习DOM主要学习： API
+var dv = document.getElementsByTagName("div")[0];
+
+// 操作DOM有哪几点：
+// CRUD 增 删 改 查
+// 查： ById / ByTagName / ByClassName / querySelector / querySelectorAll
+// 增： createElement / createTextNode创建文本节点 / innerHTML / cloneNode
+// 删： removeChild 父元素调用该方法然后移除子元素
+// 改： style / innerHTML / innerText / textContent / replaceChild
+
+
+// BOM：Browser Object Model 浏览器对象模型
+// window
+// 		location
+// 		open()   打开新窗口
+// 		screen
+// 		localStorage 本地存储
+
+
+// ECMAScript 是JavaScript语言的标准
+// 		除了DOM和BOM之外的内容都是ECMAScript的内容
